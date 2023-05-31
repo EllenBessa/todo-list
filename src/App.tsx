2869d1b2f5102ae5ae5273db5f3cccc81e75c06e
EllenBessa <ellenbessa96@gmail.com>
2023-05-31 14:54:48 -0300

    feat: :lipstick: Inserindo a criação de listas

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,36 @@ import { Task } from "./components/Task";
 import styles from "./App.module.css";
 
 import "./global.css";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export function App() {
+  const [todoList, setTodoList] = useState(["Arrumar o quarto"]);
+
+  const [newListText, setNewListText] = useState("");
+
+  function handleCreateNewList(event: FormEvent) {
+    event.preventDefault();
+
+    setTodoList([...todoList, newListText]);
+    setNewListText("");
+  }
+
+  function handleNewListChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewListText(event.target.value);
+  }
+
   return (
     <>
       <Header />
 
       <main className={styles.wrapper}>
-        <form className={styles.form}>
-          <Input placeholder="Adicione uma nova tarefa" />
+        <form onSubmit={handleCreateNewList} className={styles.form}>
+          <Input
+            name="list"
+            placeholder="Adicione uma nova tarefa"
+            value={newListText}
+            onChange={handleNewListChange}
+          />
           <Button />
         </form>
 
@@ -31,8 +52,9 @@ export function App() {
             </div>
           </header>
 
-          <Task />
-          <Task />
+          {todoList.map((todoList) => {
+            return <Task key={todoList} content={todoList} />;
+          })}
         </section>
       </main>
     </>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,20 +2,18 @@ import { Trash } from "@phosphor-icons/react";
 
 import styles from "./Task.module.css";
 
-export function Task() {
+interface ListProps {
+  content: string;
+}
+
+export function Task({ content }: ListProps) {
   return (
     <div className={styles.task}>
       <input type="checkbox" />
 
-      <span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid
-        quidem laboriosam tempore Lorem ipsum dolor sit amet consectetur,
-        adipisicing elit. Ducimus est voluptates iure voluptatem eum dolorem
-        laudantium soluta temporibus eos, quia velit praesentium incidunt!
-        Temporibus blanditiis ß{" "}
-      </span>
+      <span>{content}</span>
 
-      <button>
+      <button type="submit">
         <Trash size={20} />
       </button>
     </div>
